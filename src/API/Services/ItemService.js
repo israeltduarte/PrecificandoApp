b9d1c12deac6e5b8a15a48f585b236204a84cc89
Israel Duarte <israeltduarte@gmail.com>
2023-09-27 23:22:25 -0300

    add item service

---
+++ src/API/Services/ItemService.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://localhost:8080";
+
+class ItemService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: API_BASE_URL,
+      timeout: 5000,
+    });
+  }
+
+  async getItems() {
+    try {
+      const response = await this.api.get("/api/items");
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createItem(itemData) {
+    try {
+      const response = await this.api.post("/api/items", itemData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateItem(itemId, itemData) {
+    try {
+      const response = await this.api.put(`/api/items/${itemId}`, itemData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteItem(itemId) {
+    try {
+      const response = await this.api.delete(`/api/items/${itemId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const itemService = new ItemService();
+
+export default itemService;
